7520ce709f0c6e00b7086d034d6b694fd750056d
Emanuel Sancho <>
2021-04-24 14:07:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+  int b, h;
+  cout << "Introduce la base del triángulo: \t", cin >> b;
+  cout << "Introduce la altura del triángulo: \t", cin >> h;
+  cout <<"El área del trinagulo es: "<< b*h;
+  return 0;
+}
